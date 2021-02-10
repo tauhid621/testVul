29d92f4b25ac903f5461bd319eb38bdd8e8ee6e7
Tauhid Anjum <tauhidanjum@gmail.com>
2021-02-10 13:10:23 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  console.log("Index route");
+  res.render('index');
+});
+
+module.exports = router;
